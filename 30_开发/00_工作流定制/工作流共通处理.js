030c4fb28f2011b428ab8065ac771597249466c9
Steven Liang <1442498046@qq.com>
2019-05-13 20:31:46 +0800

    使用字典控制多个编辑flg

--- 30_开发/00_工作流定制/工作流共通处理.js
+++ 30_开发/00_工作流定制/工作流共通处理.js
@@ -7,7 +7,8 @@
  var NIBAN_NODE_KEY = 'NODE0002';
  var LEADER_NODE_KEY = 'NODE0004';
  var DEPT_NODE_KEY  = 'NODE0005';
- var editFlag = true;
+//  var editFlag = true;
+ var editFlagDict = {};
 
 /*
  * 领导意见
@@ -589,7 +590,7 @@ function addCustomButtons(cell_id, nodekey) {
     title_bar.prepend(read_li);
 }
 
-/*************************************领导意见修改************************************** */
+/*************************************审批意见修改************************************** */
 // 通用修改Cell
 function initChangeOpinionCell(MessageID){
     $.ajax({
@@ -604,6 +605,8 @@ function initChangeOpinionCell(MessageID){
                     var type = item.type;
                     var history = item.history;
                     var node = item.node;
+                    // 使用字典存储每个节点的编辑状态
+                    editFlagDict[node] = true;
                     
                     if (type == 0){ // 单人审批意见
                         $(cellID).html("");
@@ -636,65 +639,9 @@ function initChangeOpinionCell(MessageID){
 	});
 }
 
-//初始化拟办意见cell
-function initNibanCell(MessageID){
-	var nibanSelector = '#C-8-4';
-
-	$.ajax({
-		url: "/Apps/DEP/Test/GeneralOpinion?MessageID=" + MessageID,
-		type: 'GET',
-		async: false,
-		dataType:'json',
-		success: function(r) {
-			// 检查是否有最新意见
-			if(r.Opinion != undefined && r.Opinion.length > 0){
-				$(nibanSelector).html("");
-				$(nibanSelector).html(r.Opinion);
-			}
-		}
-	});
-}
-
-
- //初始化领导意见cell
- function initLeaderCell(MessageID){    
-  var leaderSelector = LEADER_OPINION_CELL_HISTORY_ID;
-  if($(leaderSelector).text() == ""){
-    leaderSelector = LEADER_OPINION_CELL_ID;
-  }
-    $(leaderSelector).html("");
-    
-    $.ajax({
-		url: "/Apps/DEP/Test/GeneralOpinion?MessageID=" + MessageID,
-        type: 'GET',
-        async: false,
-        dataType:'json',
-        success: function(r) {
-            $(leaderSelector).html(r.history);
-			// 当前用户可签批时，应隐藏签批按钮
-			if(leaderSelector === LEADER_OPINION_CELL_HISTORY_ID){
-				$(".fa.fa-edit").hide();
-			}
-        }
-    });
-    
-  }
-
-//初始化 可以编辑的区域
-function initModify(){
-    //当存在领导节点的时候，让领导意见部分可以再次修改
-    if($.inArray(LEADER_NODE_KEY,read_only_node_ids) >=0 ){
-
-        $(".my-opinion").on('click',function(){
-            addModifyTextArea(this)
-        })
-    }
-
-}
-
 //转换编辑区域
 function addModifyTextArea(opinionObj, node, type, cellId){
-    if(editFlag)
+    if(editFlagDict[node])
     {
         //首先清空该td的点击事件
         var opinionVal = $(opinionObj).find('span').text();
@@ -718,7 +665,7 @@ function addModifyTextArea(opinionObj, node, type, cellId){
     }
     else
     {
-        editFlag = true;
+        editFlagDict[node] = true;
 		// 显示编辑图标
 		$(".fa.fa-edit").show();
     }
@@ -763,7 +710,7 @@ function addCancelButton(node){
     c_button.textContent = "取消"
     c_button.setAttribute("id","btn-modify-cancel");
     c_button.onclick = function(){
-        editFlag = false;
+        editFlagDict[node] = false;
         $('.my-opinion').find('textarea').remove();
         $('.my-opinion').find('a').remove();
         $('.my-opinion').find('span').show(); 
@@ -772,94 +719,4 @@ function addCancelButton(node){
         });
     }
     return c_button
-}
-
-/************************** 拟办意见修改 *********************************/
-//初始化 可以编辑的区域
-function initNibanModify(){
-    //当存在领导节点的时候，让领导意见部分可以再次修改
-    if($.inArray(NIBAN_NODE_KEY,read_only_node_ids) >=0 ){
-        $(".my-niban-opinion").on('click',function(){
-            addNibanModifyTextArea(this)
-        })
-    }
-}
-
-//转换编辑区域
-function addNibanModifyTextArea(opinionObj){
-    if(nibanEditFlag)
-    {
-        //首先清空该td的点击事件
-        var opinionVal = $(opinionObj).find('span').text();
-        $(opinionObj).find('span').hide();
-
-        //插入textarea 编辑框
-        var tarea = $("<textarea />").width('95%').addClass("modify-niban");
-        tarea.val(opinionVal)
-        tarea.css({
-            'resize':'none',
-            'outline':'none',
-            'height':'100%'
-        })
-        $(opinionObj).append(tarea)
-        //添加提交按钮
-        $(opinionObj).append(addNibanModifyButton())
-        $(opinionObj).append(addNibanCancelButton())
-		// 隐藏编辑图标
-		$(".fa.fa-edit").hide();
-        $('.my-niban-opinion').unbind('click');
-    }
-    else
-    {
-        nibanEditFlag = true;
-		// 显示编辑图标
-		$(".fa.fa-edit").show();
-    }
-}
-
-//添加修改的保存按钮
-function  addNibanModifyButton(){
-   //创建保存按钮
-    var niban_modify_button = document.createElement('A');
-    niban_modify_button.textContent = "保存修改"
-    niban_modify_button.setAttribute("id","btn-read-only-modify");
-    niban_modify_button.onclick = function(){
-        // 流程ID
-        var MessageID = getQueryString("mid");
-        // 获取
-        var opinion = $(".modify-niban").val();
-        $.ajax({
-            url: '/Apps/DEP/Test/AddNewOpinion',
-            type: 'POST',
-            data: { mid: MessageID, opinion:opinion},
-            async: false,
-            dataType:'json',
-            success: function(r) {
-                if(r.Succeed){
-                    top.location.href = '/Apps/Workflow/Running/Open?mid={0}'.format(MessageID); 
-                }
-            }
-        });
-
-
-    }
-	return niban_modify_button
-}
-
-//添加修改的取消按钮
-function addNibanCancelButton(){
- 
-    var niban_c_button = document.createElement('A');
-    niban_c_button.textContent = "取消"
-    niban_c_button.setAttribute("id","btn-modify-cancel");
-    niban_c_button.onclick = function(){
-        nibanEditFlag = false;
-        $('.my-niban-opinion').find('textarea').remove();
-        $('.my-niban-opinion').find('a').remove();
-        $('.my-niban-opinion').find('span').show(); 
-        $(".my-niban-opinion").on('click',function(){
-            addNibanModifyTextArea(this)
-        });
-    }
-    return niban_c_button
 }
