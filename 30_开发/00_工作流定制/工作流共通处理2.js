84d1cee5a291be6c51c2d09854a219b0bea698dd
Steven Liang <1442498046@qq.com>
2019-05-14 16:12:16 +0800

    初始化自定义按钮

--- 30_开发/00_工作流定制/工作流共通处理2.js
+++ 30_开发/00_工作流定制/工作流共通处理2.js
@@ -14,8 +14,57 @@ function onSheetCheck() {
 function onAnyCellUpdate() {
 };
 
+// 每个表单的定制入口
+function main() {
+    /*
+     * 参数获取
+     */
+    // 获取节点ID
+    var nid = getQueryString("nid");
+    //当没有节点Id 所以处于只读状态 初始化按钮
+    var MessageID = getQueryString("mid");
+    // 领导的节点，自定义按钮
+    var isWkfRunnign = IsWkfRunning();
+
+    if(isWkfRunnign){
+        /*
+         * 动作改造
+         */
+        // 覆盖默认保存草稿操作
+        onSaveDraft();
+        // 个人意见修改
+        GetOpinionConfig(MessageID, nid);
+        // 初始化修改意见
+        initChangeOpinionCell(MessageID);
+        // 初始化自定义按钮
+        RenderCustomButton(MessageID);
+
+        if(nid === 'NODE0001'){
+            options = [
+                {'content': '中共无锡市委'},
+                {'content': '中共无锡市委办公室'},
+                {'content': '无锡市人民政府'},
+                {'content': '无锡市人民政府办公室'},
+                {'content': '中共江苏省委'},
+                {'content': '中共江苏省委办公厅'},
+                {'content': '江苏省人民政府'},
+                {'content': '江苏省人民政府办公厅'},
+                {'content': '中共中央国务院'},
+                {'content': '江苏省物价局 江苏调查总队'},
+                {'content': '无锡市河长制工作办公室'},
+                {'content': '无锡市太湖蓝藻打捞工作协调小组办公室'},
+                {'content': '省大气污染防治联席会议办公室'},
+                {'content': '无锡市环境保护委员会办公室'},
+                {'content': '无锡市太湖水污染防治办公室'}
+            ]
+            // 定制显示来文单位
+            ShowUnitList("unit", "165px", "362px", options, '#C-4-8', '#C-4-4');
+        }
+    }
+}
+
 /******************************************************************
- * 工具方法
+ * 工具方法 【必须】
  * ****************************************************************/
 function loadScripts(array,callback){
     var loader = function(src,handler){
@@ -49,51 +98,4 @@ function AddCustomCss() {
     $("#page").css('width', '100%');
     $("#page").css('height', '100%');
     $("#page").css('justify-content', 'center');
-}
-
-// 每个表单的定制入口
-function main() {
-    /*
-     * 参数获取
-     */
-    // 获取节点ID
-    var nid = getQueryString("nid");
-    //当没有节点Id 所以处于只读状态 初始化按钮
-    var MessageID = getQueryString("mid");
-    // 领导的节点，自定义按钮
-    var isWkfRunnign = IsWkfRunning();
-
-    if(isWkfRunnign){
-        /*
-         * 动作改造
-         */
-        // 覆盖默认保存草稿操作
-        onSaveDraft();
-        // 个人意见修改
-        GetOpinionConfig(MessageID, nid);
-        // 初始化修改意见
-        initChangeOpinionCell(MessageID);
-
-        if(nid === 'NODE0001'){
-            options = [
-                {'content': '中共无锡市委'},
-                {'content': '中共无锡市委办公室'},
-                {'content': '无锡市人民政府'},
-                {'content': '无锡市人民政府办公室'},
-                {'content': '中共江苏省委'},
-                {'content': '中共江苏省委办公厅'},
-                {'content': '江苏省人民政府'},
-                {'content': '江苏省人民政府办公厅'},
-                {'content': '中共中央国务院'},
-                {'content': '江苏省物价局 江苏调查总队'},
-                {'content': '无锡市河长制工作办公室'},
-                {'content': '无锡市太湖蓝藻打捞工作协调小组办公室'},
-                {'content': '省大气污染防治联席会议办公室'},
-                {'content': '无锡市环境保护委员会办公室'},
-                {'content': '无锡市太湖水污染防治办公室'}
-            ]
-            // 定制显示来文单位
-            ShowUnitList("unit", "165px", "362px", options, '#C-4-8', '#C-4-4');
-        }
-    }
 }
