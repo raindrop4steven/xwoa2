fa0982c8338b2c32d39174ddf75197e646afbcc6
Steven Liang <1442498046@qq.com>
2019-05-14 15:32:19 +0800

    样式从后台放到前台

--- 30_开发/00_工作流定制/工作流共通处理2.js
+++ 30_开发/00_工作流定制/工作流共通处理2.js
@@ -1,9 +1,8 @@
-/*******************************************************************
- * 变量定义
- ******************************************************************/
- var editFlagDict = {};
-
 function onSheetLoad() {
+    // 样式先载入
+    AddCustomCss();
+
+    // 功能定制跟进
     loadScripts(["/Apps/DEP/Content/sdk/sdk.js"], function(){
         main();
     });
@@ -15,6 +14,9 @@ function onSheetCheck() {
 function onAnyCellUpdate() {
 };
 
+/******************************************************************
+ * 工具方法
+ * ****************************************************************/
 function loadScripts(array,callback){
     var loader = function(src,handler){
         var script = document.createElement("script");
@@ -35,6 +37,21 @@ function loadScripts(array,callback){
     })();
 }
 
+// 通用样式修改
+function AddCustomCss() {
+    // 修复弹出框被文件选择框挡住的问题
+    var styleTag = $('<style>.DropDown { z-index: 999; } .my-niban-opinion a, .my-opinion a {display:inline-block; margin-right: 10px;border: 1px solid #337ab7;font-size: 13px;padding: 3px 5px;margin-bottom: 10px;margin-left:15px;} .my-niban-opinion textarea, .my-opinion textarea { margin-top: 3px;margin-left: 15px;} #receiveNo > ul > li:hover,div#unit > ul > li:hover {background: rgb(207,229,239);}</style>')
+
+    $('html > head').append(styleTag);
+    // 表单居中
+    $("#tbSheet").css('margin-left', '-71px');
+    $("#page").css('display', 'flex');
+    $("#page").css('width', '100%');
+    $("#page").css('height', '100%');
+    $("#page").css('justify-content', 'center');
+}
+
+// 每个表单的定制入口
 function main() {
     /*
      * 参数获取
@@ -45,12 +62,6 @@ function main() {
     var MessageID = getQueryString("mid");
     // 领导的节点，自定义按钮
     var isWkfRunnign = IsWkfRunning();
-    
-    /*
-     * 样式改造
-     */
-    // 添加全局样式
-    AddCustomCss();
 
     /*
      * 动作改造
